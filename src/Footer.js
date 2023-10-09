fee045963bff73465e12674ed0a1f9b484f929a3
Kanishkirthik <129273259+Kanishkirthik@users.noreply.github.com>
2023-10-09 09:12:23 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,3 +1,3 @@
 export default function Footer(){
-    return(<footer className="bg-danger text-white text-center text-capitalize">CopyRigths @KKBLOGS</footer>);
+    return(<footer className="bg-danger text-white text-center text-capitalize">CopyRights @KKBLOGS</footer>);
 }
